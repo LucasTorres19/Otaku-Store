bf619579b2cf90678a1651a0ac27afc76557a4c0
Lucas Torres <liqui0004@gmail.com>
2021-04-02 09:19:20 -0300

    Borrando cosas

--- src/components/footer.js
+++ src/components/footer.js
@@ -29,6 +29,7 @@ render(){
    <div style={styles.footer}>
        
        <h4 style={styles.h1}>7º4 - Diseñado por: Britez Leandro - Brandan Carlos - Torres Lucas</h4>
+       <h4>2021</h4>
    </div>
      </React.Fragment>
    )
