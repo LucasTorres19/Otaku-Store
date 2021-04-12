71b9d247551c8cce615c6cc0537ab17d3def7621
Carlos Brandan <carlosbrandan503@gmail.com>
2021-04-12 10:47:20 -0300

    Arregle el carousel tosco ese que teniamos ahora esta re lindo

--- src/components/card_carrusel.js
+++ src/components/card_carrusel.js
@@ -4,24 +4,7 @@ import "react-multi-carousel/lib/styles.css";
 import Cards from "./cards.js";
 import "./../assets/Css/card.css"
 
-const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+
   
 export default class card_carousel extends Component{
     
@@ -32,22 +15,53 @@ export default class card_carousel extends Component{
                 <div className="contenedor_title">
                 <h1 className="carousel_title">{this.props.title}</h1>
                 </div>
-                <Carousel 
-                centerMode={true}
-                swipeable={true}
-                draggable={false}
-                showDots={false}
-                responsive={responsive}
-                ssr={true} 
-                infinite={true}
-                keyBoardControl={true}
-                customTransition="all .5"
-                transitionDuration={500}
-                containerClass="carousel-container"
-                deviceType={this.props.deviceType}
-                dotListClass="custom-dot-list-style"
-                itemClass="carousel-item-padding-40-px"
-                >
+                <Carousel
+                    additionalTransfrom={0}
+                    arrows
+                    autoPlaySpeed={3000}
+                    centerMode={false}
+                    className=""
+                    containerClass="container-with-dots"
+                    dotListClass=""
+                    draggable={false}
+                    focusOnSelect={false}
+                    infinite
+                    itemClass="carousel-item-padding-40-px"
+                    keyBoardControl
+                    minimumTouchDrag={80}
+                    renderButtonGroupOutside={false}
+                    renderDotsOutside={false}
+                    responsive={{
+                      desktop: {
+                        breakpoint: {
+                          max: 3000,
+                          min: 1024
+                        },
+                        items: 4,
+                        partialVisibilityGutter: 40
+                      },
+                      mobile: {
+                        breakpoint: {
+                          max: 464,
+                          min: 0
+                        },
+                        items: 1,
+                        partialVisibilityGutter: 30
+                      },
+                      tablet: {
+                        breakpoint: {
+                          max: 1024,
+                          min: 464
+                        },
+                        items: 2,
+                        partialVisibilityGutter: 30
+                      }
+                    }}
+                    showDots={false}
+                    sliderClass=""
+                    slidesToSlide={1}
+                    swipeable
+                  >
                     <Cards titulo="The Legend of Zelda: Four Swords (Perfect Edition)" precio="1.255$" link="https://d3ugyf2ht6aenh.cloudfront.net/stores/001/389/629/products/tloz-thefourswordsadventures1-501b101f5f11be9db916180719803016-1024-10241-6780d23e2be293f73616181584961809-320-0.jpg"/>
                     <Cards titulo="Green Lantern: Ocaso Esmeralda" precio="1.350$" link="https://d3ugyf2ht6aenh.cloudfront.net/stores/001/389/629/products/green_lantern_ocaso_esmeralda_cov_arg1-00c3be80324309cba616181592450915-640-0.jpg"/>
                     <Cards titulo="Socks Autobots (Transformers)" precio="350$" link="https://d3ugyf2ht6aenh.cloudfront.net/stores/001/389/629/products/transformers1-2e20d8a068b97bf80f16057983115849-640-0.jpg"/>
