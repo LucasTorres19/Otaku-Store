527553a139c67bba422c4b5552141a7134d5e0dc
Carlos Brandan <carlosbrandan503@gmail.com>
2021-04-13 17:14:29 -0300

    agregamos una categoria

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,6 +2,7 @@ import React ,{Component} from 'react';
 import Dropdown from "./dropdown.js";
 import "./../assets/Css/nav.css";
 
+
 export default class Nav extends Component{
 
     // Agregar dropdowns
@@ -14,25 +15,29 @@ export default class Nav extends Component{
                 Fila1:"Manhua",Fila2:"Ecchi",Fila3:"Josei",Fila4:"Novelas Ligeras",Fila5:"Seinen",
                 Fila6:"Shojo",Fila7:"Shonen",Fila8:"Spokon",Fila9:"Yaoi / BL",Fila10:"Yuri",             
             },
-            Juegos:
+            juegos:
             {
                 Fila1:"Trok", Fila2:"YU-GI-OH!",Fila3:"Pokemon",Fila4:"Accesorios",
                 Fila5:"MAGIC THE GATHERING",Fila6:"Juegos de Mesa",Fila7:"Juegos Digitales",                             
             },
-            Libros:
+            libros:
             {
                 Fila1:"Cine",Fila2:"Dibujo",Fila3:"Jovenes Lectores",Fila4:"Artbooks",               
             },
-            Figuras:
+            merchandising:
             {
                 Fila1:"Peluches",Fila2:"Mcfarlane",Fila3:"Ooshies",Fila4:"Spinmaster",
-                Fila5:"Anime",Fila6:"FUNKOS POP",
+                Fila5:"Anime",Fila6:"FUNKOS POP",Fila7:"Bufandas"
             },
-            Hardware:
+            hardware:
             {
                 Fila1:"Motherboard",Fila2:"Procesadores", Fila3:"Memorias Ram", Fila4:"Almacenamiento",
                 Fila5:"Placas de Video",Fila6:"Gabinentes y Fuentes", Fila7:"Perifericos",Fila8:"Equipos y Notebooks",Fila9:"Cables",
                 
+            },
+            comics:
+            {
+                Fila1:"DC",Fila2:"Marvel"
             },
         }
         this.CrearNav = this.CrearNav.bind(this)
@@ -44,11 +49,13 @@ export default class Nav extends Component{
                 <React.Fragment>
                 <nav>
                     <div className="contenedor">
-                        <Dropdown   title="Manga" items={this.state.manga}/>
-                        <Dropdown  title="Juegos" items={this.state.Juegos}/>
-                        <Dropdown  title="Libros" items={this.state.Libros}/>
-                        <Dropdown  title="Figuras" items={this.state.Figuras}/>
-                        <Dropdown  title="Hardware" items={this.state.Hardware}/>
+                        <Dropdown  title="Manga" items={this.state.manga}/>
+                        <Dropdown  title="Comics" items={this.state.comics} />
+                        <Dropdown  title="Libros" items={this.state.libros}/>
+                        <Dropdown  title="Juegos" items={this.state.juegos}/>
+                        <Dropdown  title="Merchandising" items={this.state.merchandising}/>
+                        <Dropdown  title="Hardware" items={this.state.hardware}/>
+                        
                     </div>
                 </nav>
                 </React.Fragment>
