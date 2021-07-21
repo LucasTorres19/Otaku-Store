95275204697a205a0b60608e7ab2ff69fdf66c01
Lucas Torres <liqui0004@gmail.com>
2021-07-21 16:12:28 -0300

    cambiando el diseño del footer. Co-authored-by: Waffi503 <Waffi503@users.noreply.github.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React ,{Component} from "react";
 import "./../assets/Css/footer.css"
-
+import { FaInstagram , FaGithub , FaSteam , FaLinkedin  } from 'react-icons/fa';
 export default class footer extends Component{
 
 render(){
@@ -14,11 +14,30 @@ render(){
                 <h5 className="division">7°4</h5>
               </div>
               <div className="copy">
-                <div className="nombres" >
-                  <p className="p">Torres E.Lucas</p>
-                  <p className="p">Brandan Walser E.Carlos</p>
+
+                <div className="conteiner-names">
+
+                  <div className="container-user">
+                    <p className="p">Torres E. Lucas</p>
+                    <a href="https://www.instagram.com/_sr_luc4s_/"><FaInstagram/></a>
+                    <a href="https://github.com/LucasTorres19"><FaGithub/></a>
+                    <a href="https://steamcommunity.com/id/Lordgalucks/"><FaSteam/></a>
+                  </div>
+
+                  <div className="container-user">
+                    <p className="p">Brandan Walser E. Carlos</p>
+                    <a href="https://www.instagram.com/waffi_503/"><FaInstagram/></a>
+                    <a href="https://github.com/Waffi503"><FaGithub/></a>
+                    <a href="https://steamcommunity.com/id/Dedrick503/"><FaSteam/></a>
+                    <a href="https://www.linkedin.com/in/carlos-brandan-dev/"><FaLinkedin/></a>
+                  </div>
+
+                  <div className="conteiner-copyright">
                   <p className="h1">Copyright © 2020-2021 MQM S.R.L</p>
+                  <a href="https://github.com/LucasTorres19/Otaku-Store"><FaGithub/></a> 
+                  </div>
                 </div>
+
               </div>
               
           </div>
