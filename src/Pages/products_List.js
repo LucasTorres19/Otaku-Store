546fe5c8c819808f62f2b0f9f9ae23ceb53f5402
Lucas Torres <47805417+LucasTorres19@users.noreply.github.com>
2021-07-21 21:09:25 -0300

    Update products_List.js

--- src/Pages/products_List.js
+++ src/Pages/products_List.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 import Header from '../components/header.js';
 import Footer from "../components/footer.js";
 import Nav from "../components/nav.js";
-import List from "../components/list.js";
+import List from "./../components/list.js";
 import '../assets/Css/list.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
