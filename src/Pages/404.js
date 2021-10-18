02e6660400d0a271955c52b0a4c1604bd1f391c2
Waffi503 <carlosbrandan503@gmail.com>
2021-10-18 09:49:08 -0300

    Estilizando la pagina 404

--- src/Pages/404.js
+++ src/Pages/404.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from '../components/header.js';
+import '../assets/Css/404.css'
 export default function notfound (){
 
     return <>
-    <Header/>
-    <h1>404 not found</h1>
-    <p>Como has llegado aqui?</p>
-    
-    
-    
-    
+        <Header/>
+        <div className="container-404">
+            <h1 className="container-404__h1">404 not found</h1>
+            <p className="container-404__p">Como has llegado aqui?</p>
+            <img className="container-404__img" alt="logo not found" src="https://i.pinimg.com/originals/8b/d8/c4/8bd8c42fc38edf71fbb2f9d51cf08345.png" />
+        </div>
     </>
 
 }
